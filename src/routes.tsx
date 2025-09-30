ec8ac7a8436cfad7fec78c504cbf2b8dae8e52fa
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-09-30 12:40:53 -0300

    feat: creating main layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,14 +3,18 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Vehicles from './pages/Vehicles';
 import Contact from './pages/Contact';
+import MainLayout from './layouts/MainLayout';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Home />} index path="/" />
-        <Route element={<Vehicles />} path="/veiculos" />
-        <Route element={<Contact />} path="/contato" />
+        <Route element={<MainLayout />} path="/">
+          <Route element={<Home />} index />
+          <Route element={<Vehicles />} path="/veiculos" />
+          <Route element={<Contact />} path="/contato" />
+          <Route element={<h1>Not Found!</h1>} path="*" />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
